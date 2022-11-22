ae66cee0aed7df98956ea99d9d80d63b116b12ab
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2022-11-22 18:39:46 +0000

    Feat: Add code to access google sheets

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Main co2 score code that asks a user questions in order to estimate their
+carbon footprint
+"""
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+# SCOPE definition code provided by Code Institute
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+# Code to access Google Sheets provided by code institute
+# Begins here
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+CO2_SHEET = GSPREAD_CLIENT.open('co2_score')
+# Ends here
+
+
