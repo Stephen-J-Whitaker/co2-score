3a06c32c3cc389fac5836d41a128d5cfd8e5c038
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2022-11-23 21:59:00 +0000

    Feat: Add question_user function to run.py and call from main

--- run.py
+++ run.py
@@ -16,22 +16,32 @@ SCOPE = [
     ]
 
 # Code to access Google Sheets provided by code institute
-# Begins here
+# Code Institute code begins here
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 CO2_SHEET = GSPREAD_CLIENT.open('co2_score')
-# Ends here
+# Code Institute code ends here
 
+
+def question_user(questionnaire_details):
+    """
+    Recall each question from the questionnaire sequentially,
+     display the question to the user and record their
+     responses into a variable
+    """
+    for question in questionnaire_details["questions"]:
+        print(question.question_info)
 
 def main():
     """
     Run all program functions
     """
     questionnaire_details = questionnaire.get_questionnaire(CO2_SHEET)
-    pprint(questionnaire_details)
-    print(questionnaire_details["questions"][0].question_info)
-    print(questionnaire_details["summary"])
+    question_user(questionnaire_details)
+    # pprint(questionnaire_details)
+    # print(questionnaire_details["questions"][0].question_info)
+    # print(questionnaire_details["summary"])
 
 
 main()
