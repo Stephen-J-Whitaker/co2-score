30254ac46cfbf7caebec73ebb8be1113c63b491f
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2022-11-24 13:58:33 +0000

    Feat: Add bitmap pixel colour to escape character map to gui.py

--- gui.py
+++ gui.py
@@ -6,3 +6,14 @@ from PIL import Image
 from numpy import asarray
 from colorama import Fore, Back, Style
 
+# Map bitmap pixel colours to equivalent escape characters
+colour_map = {
+    "(0, 0, 0)": "\033[40;30m\u2588",  # black
+    "(255, 0, 0)": "\033[41;31m\u2588",  # red
+    "(0, 255, 0)": "\033[42;32m\u2588",  # green
+    "(255, 255, 0)": "\033[43;33m\u2588",  # yellow
+    "(0, 0, 255)": "\033[44;34m\u2588",  # blue
+    "(255, 0, 255)": "\033[45;35m\u2588",  # magenta
+    "(0, 255, 255)": "\033[46;36m\u2588",  # cyan
+    "(255, 255, 255)": "\033[47;37m\u2588"  # white
+}
